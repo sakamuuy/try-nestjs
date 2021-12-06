3265596dac09731d9e69d71f57bc01acc0d3cdf5
sakamuuy <sakamuu.y@gmail.com>
2021-12-06 21:39:02 +0900

    middleware tutorial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
@@ -6,10 +6,20 @@ import { CatsService } from './cats/cats.service';
 import { CatsModule } from './cats/cats.module';
 import { DatabaseModule } from './database/database.module';
 import { Database } from './database/database';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [CatsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({
+        path: 'cats',
+        method: RequestMethod.GET
+      })
+  }
+}
